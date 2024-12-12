eef5dc0fd383314c073cabb17df28fc610ff9949
Melvin Guerra <26179386+melvinguerra@users.noreply.github.com>
2024-12-12 11:41:23 +0800

    Add reverseString logic
    - Added reverseString logic
    - Removed .skip for test script

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reversedString = "";
+    for(let i = word.length - 1; i >= 0; i--){
+        reversedString = reversedString.concat(word[i]);
+        
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
