ba0da9c8ef39aff7e2b7d58c9cb0c12053df9a1c
Melvin Guerra <26179386+melvinguerra@users.noreply.github.com>
2024-12-12 14:39:23 +0800

    Remove specific elements in an Array
    - Added algorithm to remove specific elements in an Array
    - Remove .skip in test script

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...Args) {
+    let strippedArray = arr;
+    for(let i = 0; i < Args.length; i++){
+        strippedArray = strippedArray.filter(ar => ar !== Args[i]);
+  }
+  return strippedArray;
 };
 
 // Do not edit below this line
