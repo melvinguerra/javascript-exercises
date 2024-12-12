c8ff9385c40e270b702bc5b45f2314ea028a0dd1
Melvin Guerra <26179386+melvinguerra@users.noreply.github.com>
2024-12-12 17:57:41 +0800

    Add sum logic to sumAll function
    - Computes the sum of all numbers between 2 integers
    - Removed .skip in the test scripts

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if(num1 < 0 || num2 < 0){
+        return "ERROR";
+    } else if(!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR";
+    } else if (Number.isNaN(num1) || Number.isNaN(num2)) {
+        return "ERROR";
+    } else {
+        if(num1 > num2){
+            for(let x = num2; x <= num1; x++){
+                sum += x;
+            }
+        } else {
+            for(let x = num1; x <= num2; x++){
+                sum += x;
+            }
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
