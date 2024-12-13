bf0659c30fb807ed5b668b64f15c1fafc8d238b2
Melvin Guerra <26179386+melvinguerra@users.noreply.github.com>
2024-12-13 17:00:00 +0800

    Add calculator logic to functions
    - Added calculator logic to each function
    - Removed .skip to enable all tests in the test script

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(adder, addend) {
+  return adder + addend;
 };
 
-const subtract = function() {
-	
+const subtract = function(minuend, subtrahend) {
+	return minuend - subtrahend;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+	array.forEach(element => {
+    sum += element;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product= 1;
+	array.forEach(element => {
+    product *= element;
+  });
+  return product;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return Math.pow(base,exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num < 0) {
+    return -1;
+  } else if (num === 0) {
+    return 1;
+  } else {
+    return num * factorial(num-1);
+  }
 };
 
 // Do not edit below this line
