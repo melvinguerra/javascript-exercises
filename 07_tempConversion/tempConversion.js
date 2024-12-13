f9fc3aeed3294dc965e512b75514edb6d49e0f83
Melvin Guerra <26179386+melvinguerra@users.noreply.github.com>
2024-12-13 14:44:02 +0800

    Add conversion computation and 1 decimal output
    - Added conversion to Fahrenheit and Celcius computation and 1 decimal output
    - Removed .skip in test scripts to enable all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+let rounded = 0;
+const convertToCelsius = function(fahrenheit) {
+  rounded = (fahrenheit - 32) * (5 / 9);
+  return Number(rounded.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  rounded = celsius * (9 / 5) + 32;
+  return Number(rounded.toFixed(1));
 };
 
 // Do not edit below this line
