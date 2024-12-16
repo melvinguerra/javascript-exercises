0226aa01cfb92a809d29ef2b3a21d4fe713d4f01
Melvin Guerra <26179386+melvinguerra@users.noreply.github.com>
2024-12-16 14:33:53 +0800

    Add logic to check if string is a palindrome
    - Added regex to remove punctuations and whitespaces
    - Removed .skip in test script to enable all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = removePunctuationAndSpaces(word).toLowerCase();
+    for(let i = 0, j = word.length-1; i < word.length-1; i++,j--){
+        if(word[i] !== word[j]){
+            return false;
+        }
+    }
+    return true;
 };
+
+function removePunctuationAndSpaces(input) {
+    return input.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\s']/g, "");
+}
 
 // Do not edit below this line
 module.exports = palindromes;
