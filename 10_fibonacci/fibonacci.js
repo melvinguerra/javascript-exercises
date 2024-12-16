76bffd14dd9e781ff5a68866271b43fed8e4c646
Melvin Guerra <26179386+melvinguerra@users.noreply.github.com>
2024-12-16 16:32:59 +0800

    Add logic to compute fibonacci
    - Added logic to compute fibonacci
    - Remove .skip in test scrip to enable all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    
+    if(Number(num) === 0){
+        return 0;
+    } else if (num < 0) {
+        return "OOPS";
+    } else {
+    let sum = 1;
+    let prev = 0;
+    let temp = 1;
+    for(let i = 1; i < num; i++){   
+        sum = sum + prev;
+        prev = temp;
+        temp = sum;
+        
+        
+        // prev = temp;
+        
+    }
+    return sum;
+}
 };
 
 // Do not edit below this line
