696e7cd207b1fcf9db92002e1e24e28a0a927935
Melvin Guerra <26179386+melvinguerra@users.noreply.github.com>
2024-12-17 18:21:06 +0800

    Add logic to return oldest person in an array
    - Added logic to compute the oldest person in an array
    - Removed .skip in the test script to enable all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    //let oldest = people.reduce();
+    let currentYear = 2024;
+    let oldestPerson = {};
+    const oldestAge = people.reduce((currentOldest, person) => {
+        if(person.yearOfDeath === undefined) {
+            person.yearOfDeath = currentYear;
+        }
+        let age = person.yearOfDeath - person.yearOfBirth;
+        if(age > currentOldest){
+            oldestPerson = person;
+            return age;
+        } else {
+            return currentOldest;
+        }
+    }, 0)
+    return oldestPerson;  
 };
+
+//calculate the  age of each person
+//compare age of each person
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
